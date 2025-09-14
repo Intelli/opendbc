a5c9c831aedfc81af90bacc0d96d2304f2eab39c
Intelli <contact@intelli.software>
2025-09-14 15:33:55 -0600

    Update sync-upstream.sh

--- sync-upstream.sh
+++ sync-upstream.sh
@@ -1,4 +1,6 @@
-git restore --source=upstream/hkg-angle-steering-2025 --staged --worktree --no-overlay .
+git fetch upstream hkg-angle-steering-2025 --prune
+git restore --source=upstream/hkg-angle-steering-2025 --staged --worktree --no-overlay -- . ':(glob,exclude)**/AGENTS.md' ':(glob,exclude)**/sync-upstream.sh'
+#git restore --source=upstream/hkg-angle-steering-2025 --staged --worktree --no-overlay .
 #git clean -fd
 #git commit -m "Sync upstream"
 #git push origin hkg-angle-steering-2025
